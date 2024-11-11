8283480db0e89745a5320c1a32119792c0e63113
Daniru <daniil.nikiforenko03@mail.ru>
2024-11-11 12:10:40 +0700

    feat/user model

--- devnexus/devnexus/settings.py
+++ devnexus/devnexus/settings.py
@@ -138,5 +138,5 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "user.User"
 LOGIN_URL = "/user/login/"
