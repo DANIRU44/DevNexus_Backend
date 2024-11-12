ca9fe8b616520859df96a488e11161edf47ec4b5
Daniru <daniil.nikiforenko03@mail.ru>
2024-11-12 22:31:47 +0700

    feat/user prifile and login

--- devnexus/user/serializers.py
+++ devnexus/user/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from user.models import User
 
 
@@ -13,3 +14,28 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise serializers.ValidationError('Invalid username or password.')
+
+        attrs['user'] = user
+        return attrs
+    
+
