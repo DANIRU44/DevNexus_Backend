3c4276aa6ac3d182f35ce8e76258f12417ada4df
Daniru <daniil.nikiforenko03@mail.ru>
2024-11-12 21:06:58 +0700

    feat/user registration serializer and view

--- devnexus/user/views.py
+++ devnexus/user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from django.contrib.auth import login
+from .serializers import UserSerializer
+from user.models import User
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
