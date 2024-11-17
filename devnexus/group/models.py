474a38e3e429e1321d521c6171838cde8f2544e1
Daniru <daniil.nikiforenko03@mail.ru>
2024-11-17 20:13:58 +0700

    feat/модель GroupTag

--- devnexus/group/models.py
+++ devnexus/group/models.py
@@ -19,3 +19,13 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class GroupTag(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='tags')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.group.name})"
