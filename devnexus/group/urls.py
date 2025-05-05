b7792c1ee207513aeec52536a0443aa4fcb7903e
Daniru <daniil.nikiforenko03@mail.ru>
2025-05-05 12:20:14 +0700

    fix/обновление карточек

--- devnexus/group/urls.py
+++ devnexus/group/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('<str:group_uuid>/card/<str:code>/', CardDetailView.as_view(), name='card-detail'),
 
     path('<str:group_uuid>/tag/create/', GroupTagCreateView.as_view(), name='tag-create'),
-    path('<str:group_uuid>/tag/all', GroupTagListView.as_view(), name='group-tags-list'),
+    # path('<str:group_uuid>/tag/all', GroupTagListView.as_view(), name='group-tags-list'),
     path('<str:group_uuid>/tag/<str:code>/', GroupTagDetailView.as_view(), name='tag-detail'),
 
     path('<str:group_uuid>/usertag/create/', UserTagCreateView.as_view(), name='usertag-create'),
